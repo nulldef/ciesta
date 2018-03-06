ec47b9d4b0e04990c4517ee51a90ca4013c07e53
Alexey <alex.coder1@gmail.com>
2018-03-06 20:58:37 +0300

    coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 require "coveralls"
-require "simplecov"
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter,
-])
-SimpleCov.start
 Coveralls.wear!
 
 require "bundler/setup"
