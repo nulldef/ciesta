daf0896438d27c6beb65a6d4b465594119c04466
Alexander Komarov <ak@akxcv.com>
2018-10-11 16:00:00 +0300

    [Fix] Uninitialized constant Ciesta error

--- lib/ciesta/delegator.rb
+++ lib/ciesta/delegator.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 
 # Simple module for delegate methods
-module Ciesta::Delegator
-  # Delegates methods to object
-  #
-  # @param [Array<String, Symbol>] methods Methods to delegate
-  # @param [Symbol] to Method's name delegate to
-  def delegate(*methods, to:)
-    methods.each do |name|
-      method_def = [
-        "def #{name}(*args, &block)",
-        "  if !#{to}.nil?",
-        "    #{to}.#{name}(*args, &block)",
-        "  end",
-        "end"
-      ].join(";")
+module Ciesta
+  module Delegator
+    # Delegates methods to object
+    #
+    # @param [Array<String, Symbol>] methods Methods to delegate
+    # @param [Symbol] to Method's name delegate to
+    def delegate(*methods, to:)
+      methods.each do |name|
+        method_def = [
+          "def #{name}(*args, &block)",
+          "  if !#{to}.nil?",
+          "    #{to}.#{name}(*args, &block)",
+          "  end",
+          "end"
+        ].join(";")
 
-      module_eval(method_def)
+        module_eval(method_def)
+      end
     end
   end
 end
